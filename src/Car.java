2968c2aed48ca7115ec2527fdf3c0a522666cd9e
Kiryl Kunavich <kirill99hockey@gmail.com>
2023-04-17 21:21:59 +0300

    messege

--- src/Car.java
+++ src/Car.java
@@ -1,26 +1,33 @@
+import java.util.Objects;
+
 public class Car {
-    private String logo = "Audi";
-    private String model = "A6";
-    private double engineСapacity = 2.5;
-    private int yearOfManufacture = 2000;
+     String logo;
+     String model;
+     double engineСapacity;
+     int yearOfManufacture;
 
-    public String getLogo() {
-        return logo;
+    @Override
+    public String toString() {
+        return "Car{" +
+                "logo='" + logo + '\'' +
+                ", model='" + model + '\'' +
+                ", engineСapacity=" + engineСapacity +
+                ", yearOfManufacture=" + yearOfManufacture +
+                '}';
     }
 
-    public void setLogo(String logo) {
+    public Car(String logo, String model, double engineСapacity, int yearOfManufacture) {
         this.logo = logo;
-    }
-
-    public int getYearOfManufacture() {
-        return yearOfManufacture;
-    }
-
-    public void setYearOfManufacture(int yearOfManufacture) {
+        this.model = model;
+        this.engineСapacity = engineСapacity;
         this.yearOfManufacture = yearOfManufacture;
     }
-    @Override
-    public void compare(){
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return yearOfManufacture == car.yearOfManufacture && model.equals(car.model);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
-public class Main {
+public class Main{
+
     public static void main(String[] args) {
         //Условие задачи
         //Создать класс «машина» с полями: марка, модель, объём двигателя и год выпуска.
         //Переопределить метод equals, который определяет по модели и году выпуска эквивалентны ли две машины.
-        Audi audi = new Audi("A6", 2001);
 
-    }
+        Car car = new Car("Audi","a6",2.5,2000);
+        Car car1 = new Car("Audi","a6",2.5,2001);
+
+        if (car.equals(car1)){
+            System.out.println("Car compare");
+            System.out.println(car.toString());
+            System.out.println(car1.toString());
+        }else {
+            System.out.println("Car not compare");
+            System.out.println(car.toString());
+            System.out.println(car1.toString());
+        }
+   }
 }
